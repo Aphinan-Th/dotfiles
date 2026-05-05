c6aaa0a5a1ce2896b160bb751bd5d48eb5195900
Andrew <aphinan_th@kkumail.com>
2026-05-05 16:21:26 +0700

    Redesign SketchyBar to Glass Islands layout
    Two floating frosted-glass pills on a transparent bar:
    - Left island: workspace indicators with app icons + front app
    - Right island: battery + clock with green accent
    - Darker glass background, improved text visibility
    - Removed apple logo, media, volume, and CPU items

--- config/.config/sketchybar/items/battery.sh
+++ config/.config/sketchybar/items/battery.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 sketchybar --add item battery right \
-           --set battery update_freq=120 \
-                         script="$PLUGIN_DIR/battery.sh" \
-           --subscribe battery system_woke power_source_change
+  --set battery \
+  icon.font="SF Pro:Semibold:14.0" \
+  label.font="SF Pro:Semibold:12.0" \
+  background.drawing=off \
+  update_freq=120 \
+  script="$PLUGIN_DIR/battery.sh" \
+  --subscribe battery system_woke power_source_change
